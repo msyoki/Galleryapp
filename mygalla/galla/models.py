e9b36892327da26bb85e428fe4e77e02811758f6
msyoki <msyokimutua@gmail.com>
2020-10-09 12:22:31 +0300

    add models

---
+++ mygalla/galla/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.MOdel):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    images = models.ImageField(upload_to = 'photos/',null =True)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    category= models.ForeignKey('Category',on_delete = models.CASCADE, null='True', blank=True)
+    location= models.ForeignKey('Location',on_delete = models.CASCADE, null='True', blank=True)
+
+    def __str__(self):
+        return self.name
