60ca42c4e2a2ffffb78fb38c9abf082d3befe2fa
msyoki <msyokimutua@gmail.com>
2020-10-09 13:02:44 +0300

    register new models in admin

--- mygalla/galla/admin.py
+++ mygalla/galla/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category,Location,Image
 
 # Register your models here.
+
+admin.register(Category)
+admin.register(Location)
+admin.register(Image)
