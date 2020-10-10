95893e7aa528c0d840490badd0480e73313efd54
msyoki <msyokimutua@gmail.com>
2020-10-10 10:01:53 +0300

    add bootstrap and static root

--- mygalla/mygalla/settings.py
+++ mygalla/mygalla/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galla',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
