4c8cbbe4ffedd862792239e3efef1be5841f5097
msyoki <msyokimutua@gmail.com>
2020-10-10 09:59:36 +0300

    setup image view function

--- mygalla/galla/views.py
+++ mygalla/galla/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+
+def galla(request):
+    all_photos = Image.all_photos()
+    print(all_photos)
+    return render(request, 'index.html',{"all_photos":all_photos})
