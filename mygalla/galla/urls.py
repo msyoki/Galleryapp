613ddb3f22d69bedd5fc3eebd82ef28da90988af
msyoki <msyokimutua@gmail.com>
2020-10-11 02:27:52 +0300

    add search url

--- mygalla/galla/urls.py
+++ mygalla/galla/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.galla,name='galla'),
+    url(r'^search/', views.search_category, name='search_category'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
