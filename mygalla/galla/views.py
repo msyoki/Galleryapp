1538f4c40f7f29933a95eb1049ce089c017164cb
msyoki <msyokimutua@gmail.com>
2020-10-11 02:27:35 +0300

    add search function in views

--- mygalla/galla/views.py
+++ mygalla/galla/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Image
+from .models import Image,Category
 
 # Create your views here.
 
@@ -7,3 +7,16 @@ def galla(request):
     images = Image.get_all_images()
     test='Gallery'
     return render(request, 'index.html',{'images': images,'test': test})
+
+def search_category(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request,'category.html',{"message":message, "images":searched_images, "category":search_term})
+    
+    else:
+        message = "Please search category"
+        
+        return render(request, 'category.html', {"message":message})
